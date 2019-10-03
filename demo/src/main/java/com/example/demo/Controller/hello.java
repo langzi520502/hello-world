8ff1b4b552a647fb106e581766933df399ef151f
langzi <673263353@qq.com>
2019-10-03 11:10:41 +0800

    hello

--- demo/src/main/java/com/example/demo/Controller/hello.java
+++ demo/src/main/java/com/example/demo/Controller/hello.java
@@ -1,4 +1,19 @@
 package com.example.demo.Controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
 public class hello {
+
+
+    @RequestMapping("/hello")
+    public String hello(){
+
+        return "hello langzi";
+    }
+
 }
